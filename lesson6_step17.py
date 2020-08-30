7822e840e058f10b893a18e1f1983e9bf9e35bb8
ЗУБ <shegkryeui387t5iseu@mail.ru>
2020-08-30 16:27:59 +0300

    waiting for the desired text

---
+++ lesson6_step17.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import math
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+try: 
+    link = "http://suninjuly.github.io/explicit_wait2.html"
+    browser = webdriver.Chrome()
+       
+    browser.get(link)
+    # говорим Selenium проверять в течение 12 секунд, пока кнопка не станет кликабельной
+    WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+    browser.execute_script("window.scrollBy(0, 100);")
+    button = browser.find_element_by_xpath("//button[contains(text(),'Book')]")
+    button.click()
+    input5 = browser.find_element_by_xpath("//span[@id='input_value']")
+    x=input5.text
+    y=calc(x)
+    input4 = browser.find_element_by_xpath("//input[@id='answer']")
+    input4.send_keys(y)
+    input2 = browser.find_element_by_xpath("//button[@type='submit']")
+    input2.click()
+    #Код ниже копирует ответ сайта и выводит его в сообщение bush
+    message = browser.find_element_by_id("verify_message")
+
+    assert "successful" in message.text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
